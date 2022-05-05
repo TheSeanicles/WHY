85e285656472b26c101ed95d89d0e12667dc4d09
TheSeanicles <60439801+TheSeanicles@users.noreply.github.com>
2022-05-05 03:33:42 +0000

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,35 +1,19 @@
 from scapy.all import *
 import time
 
-src = "128.173.54.143"
+src1 = "128.173.54.143"
 
 dst = "10.0.0.2"
 
-send(IP(src=src, dst=dst)/TCP(dport=20, flags="S"), count=20)
+send(IP(src=src1, dst=dst)/TCP(dport=20, flags="S"), count=20)
 time.sleep(5)
 
-src = "128.173.54.101"
+src2 = "128.173.54.101"
 
-send(IP(src=src, dst=dst)/ICMP(type=4, chksum=100), count=35)
+send(IP(src=src2, dst=dst)/ICMP(type=4, chksum=100), count=35)
 time.sleep(5)
 
-send(IP(src=src, dst=dst)/TCP(dport=32700, flags="SFR")/"team17")
-time.sleep(2)
-send(IP(src=src, dst=dst)/TCP(dport=32701, flags="SFR")/"team17")
-time.sleep(2)
-send(IP(src=src, dst=dst)/TCP(dport=32702, flags="SFR")/"team17")
-time.sleep(2)
-send(IP(src=src, dst=dst)/TCP(dport=32703, flags="SFR")/"team17")
-time.sleep(2)
-send(IP(src=src, dst=dst)/TCP(dport=32704, flags="SFR")/"team17")
-time.sleep(2)
-send(IP(src=src, dst=dst)/TCP(dport=32705, flags="SFR")/"team17")
-time.sleep(2)
-send(IP(src=src, dst=dst)/TCP(dport=32706, flags="SFR")/"team17")
-time.sleep(2)
-send(IP(src=src, dst=dst)/TCP(dport=32707, flags="SFR")/"team17")
-time.sleep(2)
-send(IP(src=src, dst=dst)/TCP(dport=32708, flags="SFR")/"team17")
-time.sleep(2)
-send(IP(src=src, dst=dst)/TCP(dport=32709, flags="SFR")/"team17")
-time.sleep(2)
+port = 32700
+for i in range(10):
+    send(IP(src=src2, dst=dst)/TCP(sport=port+i, flags="SFR")/"team17")
+
